78e0cbe1416cd1c08ed10c9f6bd9550493092910
Maria Karlsson <karlssonmaria1984@gmail.com>
2016-05-29 12:16:01 +0200

    Loading a scene from Start by pressing Enter working.

--- MAJA/Assets/Scripts/StartGame.cs
+++ MAJA/Assets/Scripts/StartGame.cs
@@ -13,7 +13,12 @@ public class StartGame : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
+        if (Input.GetButtonDown("Submit"))
+        {
+
         SceneManager.LoadScene(1);
+
+        }
 	
 	}
 }
