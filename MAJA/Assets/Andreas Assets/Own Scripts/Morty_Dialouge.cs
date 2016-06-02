7688858e42bd4b77d4152db799bd32653604b550
unknown <bergan@3se-spel-02.utb.fuv.se>
2016-06-02 15:36:49 +0200

    Finished Dialouge with Morty

--- MAJA/Assets/Andreas Assets/Own Scripts/Morty_Dialouge.cs
+++ MAJA/Assets/Andreas Assets/Own Scripts/Morty_Dialouge.cs
@@ -6,15 +6,74 @@ public class Morty_Dialouge : MonoBehaviour {
     public string[] Player;
     public string[] Morty;
     bool DisplayDialouge = false;
+    bool Greetings = false;
     bool FinishedTalk = false;
 
     void OnGUI()
     {
         GUILayout.BeginArea(new Rect(700, 600, 400, 400));  // Location of the GUI
-        if (DisplayDialouge && !FinishedTalk)
+        if (DisplayDialouge && !Greetings && !FinishedTalk)
         {
             GUILayout.Label(Morty[0]);
+            if (GUILayout.Button(Player[0]))
+            {
+                Greetings = true;
+                GUILayout.Label(Morty[1]);
+
+                if (GUILayout.Button(Player[1]))
+                {
+                    GUILayout.Label(Morty[2]);
+
+                    if (GUILayout.Button(Player[2]))
+                    {
+                        FinishedTalk = true;
+                        GUILayout.Label(Morty[3]);
+
+                        if (GUILayout.Button(Player[3]))
+                        {
+                            DisplayDialouge = false;
+                        }
+                    }
+                }
+            }
         }
+
+        if (DisplayDialouge && Greetings && !FinishedTalk)
+        {
+            GUILayout.Label(Morty[1]);
+
+            if (GUILayout.Button(Player[1]))
+            {
+                GUILayout.Label(Morty[2]);
+
+                if (GUILayout.Button(Player[2]))
+                {
+                    FinishedTalk = true;
+                    GUILayout.Label(Morty[3]);
+
+                    if (GUILayout.Button(Player[3]))
+                    {
+                        DisplayDialouge = false;
+                    }
+                }
+            }
+        }
+
+        if (DisplayDialouge && Greetings && FinishedTalk)
+        {
+            if (GUILayout.Button(Player[2]))
+            {
+                FinishedTalk = true;
+                GUILayout.Label(Morty[3]);
+
+                if (GUILayout.Button(Player[3]))
+                {
+                    DisplayDialouge = false;
+                }
+            }
+        }
+
+        GUILayout.EndArea();
 
     }
 
