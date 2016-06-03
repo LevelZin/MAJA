c85e9e286515909246410de110269dfed98ddaa9
unknown <bergan@3se-spel-02.utb.fuv.se>
2016-06-03 14:38:48 +0200

    movement script

---
+++ MAJA/Assets/Andreas Assets/Own Scripts/Movement.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Movement : MonoBehaviour
+{
+    [SerializeField]
+    private float Speed = 10;   // Characters move speed
+
+    void FixedUpdate()
+    {
+        Vector3 input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));   // Button input
+        GetComponent<Rigidbody>().AddForce(input * Speed);  // Move in the selected direction
+
+        if (input != Vector3.zero)  // If character has not moved
+        {
+            transform.rotation = Quaternion.LookRotation(input);    // rotate in direction pressed
+        }
+    }
+
+}
